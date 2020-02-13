61d9bdf204e3c2e29bdad4f019031ace4e39cd17
HUNG <HUNG@192.168.1.16>
2020-02-13 23:02:39 +0700

    initial commit

---
+++ src/main/java/com/project/english/gemmy/GemmyApplication.java
@@ -0,0 +1,13 @@
+package com.project.english.gemmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/english/gemmy/GemmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.english.gemmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
