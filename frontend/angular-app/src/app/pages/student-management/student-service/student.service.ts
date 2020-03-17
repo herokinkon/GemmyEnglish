9a3072b300a05984eb54dc27ade2e3dab162f2da
herokinkon <kinkon_2008@yahoo.com>
2020-03-17 22:25:56 +0700

    Add Restapi for student

--- frontend/angular-app/src/app/pages/student-management/student-service/student.service.ts
+++ frontend/angular-app/src/app/pages/student-management/student-service/student.service.ts
@@ -1,10 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AppConstant } from 'src/app/shared/app-constant.service';
+import { Observable } from 'rxjs';
+import { Student } from './student';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
+  apiUrl = AppConstant.serverURL + '/student';
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
   constructor(private http: HttpClient) { }
+
+  getStudents(): Observable<Student> {
+    return this.http.get<Student>(this.apiUrl + '/getAllStudent', this.httpOptions);
+  }
+
+  getStudent(id: number): Observable<Student> {
+    return this.http.get<Student>(this.apiUrl + `/${id}`, this.httpOptions);
+  }
+
+  deleteStudent(id: number): Observable<Student> {
+    return this.http.delete<Student>(this.apiUrl + `/${id}`, this.httpOptions);
+  }
+
+  createStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(this.apiUrl, student, this.httpOptions);
+  }
+
+  updateStudent(student: Student): Observable<Student> {
+    return this.http.put<Student>(this.apiUrl, student, this.httpOptions);
+  }
 }
